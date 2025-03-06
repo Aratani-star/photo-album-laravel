1f7c351c4c8f6b6ba3aec350b33d3bb81c70ca04
Aratani-star <masahiroaratani387@gmail.com>
2025-03-06 15:56:43 +0900

    fix get all images api

--- app/Http/Controllers/Gallery/GalleryController.php
+++ app/Http/Controllers/Gallery/GalleryController.php
@@ -24,10 +24,6 @@ class GalleryController extends Controller
         return Inertia::render('gallery', [
             'images' => $images
         ]);
-        // return Inertia::render('profile/', [
-        //     'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
-        //     'status' => $request->session()->get('status'),
-        // ]);
     }
 
     /**
