83735983fc1cec54d90fa39cfcea917c818c1479
Aratani-star <masahiroaratani387@gmail.com>
2025-03-12 08:38:56 +0900

    feat/ add modal and workflows

---
+++ app/Domain/Image/Models/Image.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Domain\Image\Models;
+
+class Image
+{
+    // Existing properties and methods
+ 
+    public function processImage()
+    {
+        // Logic for processing the image
+    }
+}
+// Compare this snippet from app/Domain/Image/Repositories/ImageRepository.php:
+/**
+ */
